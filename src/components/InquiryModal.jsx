90175b8b75c29be4f72c776e2cc0834d48394c1b
dhairyashukla08 <dhairyvshukla@gmail.com>
2026-02-23 23:37:09 +0530

    Added redirects and production API URL

--- src/components/InquiryModal.jsx
+++ src/components/InquiryModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios"; 
 import { toast } from "react-toastify";
-import {API} from "../api/axios.js"
+import API from "../api/axios.js";
 
 const InquiryModal = ({
   isOpen,
